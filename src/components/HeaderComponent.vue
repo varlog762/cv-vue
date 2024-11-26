e3bc0153c90a3e4bddf6f18d9b2f038accccc014
Gregory Pereverzev <boot762@gmail.com>
2024-11-26 13:04:53 +0300

    feat: add HeaderComponent

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -5,7 +5,31 @@ defineOptions({
 </script>
 
 <template>
-  <div></div>
+  <header class="header">
+    <div class="wrapper">
+      <div class="header_container">
+        <div class="photo"></div>
+        <div class="info">
+          <h2 class="position">Frontend разработчик (Junior)</h2>
+          <h1 class="name">Переверзев<br />Григорий<br />Владимирович</h1>
+        </div>
+        <div class="contacts">
+          <div class="email contact_item">
+            <div class="mail_icon contact_icon"></div>
+            <div class="mail_address">greg.pereverzev@gmail.com</div>
+          </div>
+          <div class="github contact_item">
+            <div class="github_icon contact_icon"></div>
+            <div class="github_address">github.com/varlog762</div>
+          </div>
+          <div class="phone contact_item">
+            <div class="phone_icon contact_icon"></div>
+            <div class="phone_number">+375 29 562 59 28</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </header>
 </template>
 
 <style lang="scss" scoped></style>
